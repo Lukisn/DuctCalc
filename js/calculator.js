1e826ced0750ec863659bd4ee84c4876ba9d4b26
Lukas Reimer <lukas.reimer@icloud.com>
2022-05-29 10:21:26 +0200

    added jsdoc docstrings to the functions

--- js/calculator.js
+++ js/calculator.js
@@ -6,9 +6,12 @@ import {
   pressureDropUnits
 } from './units.js'
 
-const ROUGHNESS = math.unit(0.1, 'mm')
-// https://de.wikipedia.org/wiki/Rohrreibungszahl
+// Constants
+const MAXITERATIONS = 100
+const MAXDIFFERENCE = 1e-6
+const ROUGHNESS = math.unit(0.1, 'mm') // https://de.wikipedia.org/wiki/Rohrreibungszahl
 
+// DOM handles
 const widthField = document.querySelector('input#width')
 const widthUnit = document.querySelector('select#widthUnit')
 const heightField = document.querySelector('input#height')
@@ -33,53 +36,86 @@ const pressureDropField = document.querySelector('input#pressureDrop')
 const pressureDropUnit = document.querySelector('select#pressureDropUnit')
 
 // Funtions
-function calcFrictionFactor (reynolds, hydraulicDiameter, roughness, maxIterations = 100, maxDifference = 1e-6) {
-  // https://de.wikipedia.org/wiki/Rohrreibungszahl
-  // 1 / √λ = -2 * log10([2.51 / (Re * √λ)] + [k / (3.71 * dh)])
-  // √λ = 1 / -2 * log10(...)
-  // λ = (1 / -2 * log10(...))²
-  // Setup dimensionless variables for calculation
+/**
+ * Calculate the friction factor from values with untis using
+ * {@link calculateFrictionFactor}.
+ * @param {*} reynolds - reynolds number of the flow (unitless)
+ * @param {*} hydraulicDiameter - hydraulic diameter of the conduit (length)
+ * @param {*} roughness - relative roughness of the conduit (length)
+ * @param {*} maxIterations - maximum number of iterations to perform
+ * @param {*} maxDifference - maximum difference to allow for stopping the
+ * iteration
+ * @returns friction factor (unitless)
+ */
+function calculateFrictionFactorWithUnits (
+  reynolds, hydraulicDiameter, roughness, maxIterations = MAXITERATIONS,
+  maxDifference = MAXDIFFERENCE) {
   const re = reynolds
-  const commonSIUnit = 'm'
+  const commonSIUnit = 'm' // must be equal for both length measurements
   const dh = hydraulicDiameter.toNumber(commonSIUnit)
   const k = roughness.toNumber(commonSIUnit)
-  // Perform calculation
-  if (re < 2300) { // laminar
-    return 64 / re
-  } else { // turbulent
+  return calculateFrictionFactor(re, dh, k, maxIterations, maxDifference)
+}
+
+/**
+ * Calculate the friction factor from unitless values by iteration.
+ * https://de.wikipedia.org/wiki/Rohrreibungszahl
+ * https://en.wikipedia.org/wiki/Darcy_friction_factor_formulae
+ * Colebrook-White Equation:
+ * 1 / √λ = -2 * log10([2.51 / (Re * √λ)] + [k / (3.71 * dh)])
+ * rearranging for λ (iteration equation):
+ * λ = (1 / -2 * log10([2.51 / (Re * √λ)] + [k / (3.71 * dh)]))²
+ * The implicit units of the values for the hydraulic diameter and roughness
+ * must be matching.
+ * @param {number} reynolds - reynolds number of the flow (unitless)
+ * @param {number} hydraulicDiameter - hydraulic diameter of the conduit (e.g. in m)
+ * @param {number} roughness - relative roughnes of the conduit walls (e.g. in m)
+ * @param {number} maxIterations - maximum number of iterations to perform
+ * @param {number} maxDifference - maximum difference to allow for stopping the iteration
+ * @returns friction factor (unitless)
+ */
+function calculateFrictionFactor (
+  reynolds, hydraulicDiameter, roughness, maxIterations = MAXITERATIONS,
+  maxDifference = MAXDIFFERENCE) {
+  if (reynolds < 2300) { // laminar flow
+    return 64 / reynolds
+  } else { // turbulent flow
     let lambda = 0.02
     let previousLambda = 0.02
     let difference = Infinity
     let iteration = 0
     do {
       iteration++
-      const leftTerm = 2.51 / (re * Math.sqrt(previousLambda))
-      const rightTerm = k / (3.71 * dh)
+      const leftTerm = 2.51 / (reynolds * Math.sqrt(previousLambda))
+      const rightTerm = roughness / (3.71 * hydraulicDiameter)
       const logTerm = Math.log10(leftTerm + rightTerm)
-      // console.log(`left = ${leftTerm}, right = ${rightTerm}, log = ${logTerm}`)
       lambda = Math.pow(1 / (-2 * logTerm), 2)
       difference = Math.abs(lambda - previousLambda)
+      // Console logging outputs for debugging:
+      // console.log(`left = ${leftTerm}, right = ${rightTerm}, log = ${logTerm}`)
       // console.log(`iteration ${iteration}: λ(i-1) = ${previousLambda}, λ(i) = ${lambda}, ∆λ = ${difference}`)
       previousLambda = lambda // swap for next iteration
     } while (iteration < maxIterations && difference >= maxDifference)
     return lambda
   }
 }
-
+/**
+ * Format a floating point number to the specified precision (number of significant digits).
+ * @param {number} original - original number to be formatted
+ * @param {number} precision - precision (number of significant digits)
+ * @returns formatted number with specified precision
+ */
 function formatNumber (original, precision = 3) {
-  var numberOfDecimalPlaces = 0
-  if (original !== 0) {
-    numberOfDecimalPlaces = -Math.log10(original) + precision
-  }
-  if (numberOfDecimalPlaces < 0) {
-    numberOfDecimalPlaces = 0
-  }
-  if (numberOfDecimalPlaces > 100) {
-    numberOfDecimalPlaces = 100
-  }
-  return parseFloat(original).toFixed(numberOfDecimalPlaces)
+  let numberOfDecimalPlaces = Math.ceil(-Math.log10(original)) + precision
+  if (numberOfDecimalPlaces === Infinity) { numberOfDecimalPlaces = 0 } // catch 0
+  if (numberOfDecimalPlaces < 0) { numberOfDecimalPlaces = 0 } // lower threshold for very large numbers
+  if (numberOfDecimalPlaces > 10) { numberOfDecimalPlaces = 10 } // upper threshold for very small numbers
+  return original.toFixed(numberOfDecimalPlaces)
 }
 
+/**
+ * Perform the calculation by reading the inputs, calculating and writing the outputs.
+ */
 function calculate () {
   // read inputs
   const width = math.unit(parseFloat(widthField.value), widthUnit.value)
@@ -92,10 +128,9 @@ function calculate () {
   const hydraulicDiameter = math.divide(math.multiply(4, crossSection), circumference) // 4 * crossSection / circumference
   const velocity = math.divide(flow, crossSection) // flow / crossSection
   const reynolds = math.divide(math.multiply(velocity, hydraulicDiameter), air.kinematicViscosity) // velocity * hydraulicDiameter / kinematicViscosity
-  const frictionFactor = calcFrictionFactor(reynolds, hydraulicDiameter, ROUGHNESS)
+  const frictionFactor = calculateFrictionFactorWithUnits(reynolds, hydraulicDiameter, ROUGHNESS)
   const frictionLoss = math.multiply(math.divide(frictionFactor, hydraulicDiameter), math.divide(air.density, 2), math.pow(velocity, 2)) // frictionFactor / hydraulicDiameter * density / 2 * velocity^2
   const pressureDrop = math.multiply(frictionLoss, length) // frictionLoss * length
-
   // write outputs
   crossSectionField.value = formatNumber(crossSection.toNumber(crossSectionUnit.value))
   circumferenceField.value = formatNumber(circumference.toNumber(circumferenceUnit.value))
@@ -107,8 +142,11 @@ function calculate () {
   pressureDropField.value = formatNumber(pressureDrop.toNumber(pressureDropUnit.value))
 }
 
+/**
+ * Setup the user interface by setting default values, filling the select options and hooking up the event handlers.
+ */
 function setup () {
-  // set initial values
+  // Set initial values
   // Width row
   widthField.value = 1.0
   widthField.min = 0.1
@@ -141,8 +179,7 @@ function setup () {
   frictionLossUnits.forEach(unit => frictionLossUnit.options.add(new Option(unit)))
   // Pressure Drop row
   pressureDropUnits.forEach(unit => pressureDropUnit.options.add(new Option(unit)))
-
-  // register event handlers
+  // Register event handlers
   widthField.onchange = calculate
   widthUnit.onchange = calculate
   heightField.onchange = calculate
--- js/units.js
+++ js/units.js
@@ -1,3 +1,4 @@
+// TODO: split into internal value adn external presentation: e.g. m^2 and m²
 export const lengthUnits = ['m', 'dm', 'cm', 'mm']
 export const areaUnits = ['m^2', 'dm^2', 'cm^2', 'mm^2']
 export const flowUnits = ['m^3/s', 'm^3/h']
